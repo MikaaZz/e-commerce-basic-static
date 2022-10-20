8368ee54efa49ea0191e26a530108ef99f1e072d
MikaaZz <mikhael928@gmail.com>
2022-10-19 21:40:21 -0300

    Adding css modules and scss

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Cardapio.module.scss';
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav>
+        <h1>Criando projeto</h1>
+      </nav>
+    </main>
+  );
+}
